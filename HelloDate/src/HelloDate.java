fbbf2136f2c12c4eb09421619ecd90baf614a1b1
Radina <r_nuneva@abv.bg>
2021-03-05 16:46:14 +0200

    added wtf

--- HelloDate/src/HelloDate.java
+++ HelloDate/src/HelloDate.java
@@ -4,5 +4,6 @@ public class HelloDate {
     public static void main(String[] args) {
         System.out.println("Hello, it's: ");
         System.out.println(new Date());
+        System.out.println("wtf");
     }
 }
